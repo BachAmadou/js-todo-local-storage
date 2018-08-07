f8a2fcd938e9407f8c5d4671077da7d8d83bcee1
Bach <bafosth@yahoo.fr>
2018-08-06 20:03:40 -0400

    add functionality to js

--- script.js
+++ script.js
@@ -1,18 +1,21 @@
 
 var form = document.querySelector('form');
-var todoList = document.querySelector('ul');
+var todosList = document.querySelector('ul');
 var button = document.querySelector('button');
 var input = document.getElementById('user-todo');
 
 // Declare variable `todosArray` to hold our todos.
 // set our `todosArray` to an empty array.
-var todosArray = [];  
+// var todosArray = []; 
 
+// We want to ask if there are already `todos` in localStorage. If so, then we will get
+ // those `todos` using JSON.parse. If local storage does not have any `todos` then we // will set our `todosArray` to an empty array.
+var todosArray = localStorage.getItem('todos') ? JSON.parse(localStorage.getItem('todos')) : [];
 
 // use localStorage setItem() method to set `todos` to a string with `JSON.stringify()`
   // JSON.stringify() is used because localStorage works with strings. This is how
   // we send data to localStorage.
-localStorage.setItem('todo', JSON.stringify(todosArray));
+localStorage.setItem('todos', JSON.stringify(todosArray));
 
 
 // Declare a variable `storage` that contains all the information in localStorage.
@@ -20,7 +23,7 @@ localStorage.setItem('todo', JSON.stringify(todosArray));
   // storage into data we can access with JavaScript.
   // When receiving data from a web server, the data is always a string.
   //Parse the data with JSON.parse(), and the data becomes a JavaScript object.
-var storage = JSON.parse(localStorage.getItem('todo'));
+var storage = JSON.parse(localStorage.getItem('todos'));
 
 form.addEventListener('submit', function (e) {
   e.preventDefault();
@@ -30,15 +33,15 @@ form.addEventListener('submit', function (e) {
 
   // on localStorage now use `setItem()` for the key `'todos'` the value
  // of the todosArray as a string with the `JSON.stringify()` method.
- localStorage.setItem('todo', JSON.stringify(todosArray));
+ localStorage.setItem('todos', JSON.stringify(todosArray));
   todoMaker(input.value);
   input.value = "";
 });
 
 var todoMaker = function(text) {
-  var todo = document.createElement('li');
-  todo.textContent = text;
-  todoList.appendChild(todo);
+  var todos = document.createElement('li');
+  todos.textContent = text;
+  todosList.appendChild(todos);
 }
 
 // Loop through localStorage when a user first opens a page and run the todoMaker function
@@ -47,8 +50,8 @@ button.addEventListener('click', function() {
 
   // clear the `localStorage` with the `clear()` method
   
-  while (todoList.firstChild) {
-    todoList.removeChild(todoList.firstChild);
+  while (todosList.firstChild) {
+    todosList.removeChild(todosList.firstChild);
   }
 });
 
